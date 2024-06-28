abfe5fefdcf7a6af98f6b17d6a1ca16af8df7e99
Arijeet <arijeetsarkar@makerstudio.io>
2024-06-28 18:51:02 +0530

    feat: add breadcrumbs ui

---
+++ src/lib/components/ui/breadcrumbs/breadcrumb.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+    	import { onMount } from 'svelte';
+	import { page } from '$app/stores';
+	let currentPath = '';
+	let firstPath = '';
+	let secondPath = '';
+
+	onMount(() => {
+		currentPath = $page.url.pathname;
+
+		if (currentPath) {
+			const parts = currentPath.split('/');
+			if (parts.length >= 3) {
+				firstPath = parts[1];
+				secondPath = parts[2];
+			}
+		}
+	});
+</script>
+
+<section class="flex items-center gap-4 font-medium">
+    <div class="text-surface-500">{firstPath.charAt(0).toUpperCase() + firstPath.slice(1)}</div>
+    <div>{'>'}</div>
+    <div class="text-primary-800">{decodeURIComponent(secondPath.charAt(0).toUpperCase() + secondPath.slice(1))}</div>
+</section>
